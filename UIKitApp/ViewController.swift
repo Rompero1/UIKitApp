663a073f1b905d13d2f678db7ffb50d487a9c857
Vitaly <sga.vitaliy@gmail.com>
2020-05-16 18:06:30 +0300

    Initial Commit

---
+++ UIKitApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIKitApp
+//
+//  Created by Vitaly Kuryatnik on 16.05.2020.
+//  Copyright © 2020 Vitaly Kuryatnik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
